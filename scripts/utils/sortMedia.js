1908fb6801c16885ecf51bcf78c758707de8b0c9
basthub <gallo.bastien@outlook.com>
2022-06-15 23:02:45 +0200

    add keyboard detection on sortMedia

--- scripts/utils/sortMedia.js
+++ scripts/utils/sortMedia.js
@@ -7,13 +7,15 @@ class SortMedia{
     }
 
     render(){
-        const sortMenu = document.querySelector(".galerie_filter");
+        const sortMenu = document.querySelector("#filters");
         const filterActive = document.querySelector(".filter--selected");
-        const filterWrapped = document.querySelector(".filter_wrapped");
+        const filterWrapped = document.querySelectorAll(".filter_wrapped"); 
         
 
         sortMenu.addEventListener("click", (event) => {
-            filterWrapped.classList.toggle("filter_wrapped--active");
+            for (const filterWrap of filterWrapped){
+                filterWrap.classList.toggle("filter_wrapped--active");
+            }
             
             let filterSelected = event.target.innerHTML;
             
@@ -27,32 +29,54 @@ class SortMedia{
                 const lightboxRender = new Lightbox(this.medias, this.gallery);
                 lightboxRender.render();
                 const likesRender= new Likes(this.medias);
-                likesRender.render();
-                               
+                likesRender.render();                 
             }   
         }); 
+        
+        sortMenu.addEventListener('keydown', (e) =>{
+            if(e.key ==='Enter'){
+                for (const filterWrap of filterWrapped){
+                    filterWrap.classList.toggle("filter_wrapped--active");
+                }
+                    
+                let filterSelected = e.target.innerHTML;
+                    
+                const filters = ['Date', 'Titre', 'Popularité'];
+        
+                if (filters.includes(filterSelected)){
+                    e.target.innerHTML = filterActive.innerHTML;
+                    filterActive.innerHTML = filterSelected;
+                    this.sortBy(filterSelected);
+                    this.gallery.render();
+                    const lightboxRender = new Lightbox(this.medias, this.gallery);
+                    lightboxRender.render();
+                    const likesRender= new Likes(this.medias);
+                    likesRender.render();            
+                }   
+            }
+        });
     }
     
     sortBy(sortType){
         switch (sortType){
             case 'Popularité':
-                console.log("switch like "+sortType);
+//                console.log("switch like "+sortType);
                 return this.sortByLike();
             case 'Titre':
-                console.log("switch title "+ sortType);
+//                console.log("switch title "+ sortType);
                 return this.sortByTitle();
             case 'Date':
-                console.log("switch date "+ sortType);
+//                console.log("switch date "+ sortType);
                 return this.sortByDate();
             default:
-                console.log("switch default "+ sortType);
-                console.log(sortType);
+//                console.log("switch default "+ sortType);
+//                console.log(sortType);
                 return this.sortByLike();
             }  
     }
 
     sortByLike(){
-        console.log("sort by like");
+//        console.log("sort by like");
         this.gallery.filteredMedias.sort(function compare(a, b){
             if (a.likes < b.likes) {
                 return -1;
@@ -68,7 +92,7 @@ class SortMedia{
     }
 
     sortByTitle(){
-        console.log("sort by title");
+//        console.log("sort by title");
         this.gallery.filteredMedias.sort(function compare(a, b){
             if (a.title < b.title){
                 return -1;
@@ -83,7 +107,7 @@ class SortMedia{
     }
     
     sortByDate(){
-        console.log("sort by date");
+//        console.log("sort by date");
         this.gallery.filteredMedias.sort((a, b) => {
             return new Date(b.date) - new Date(a.date)
         });
